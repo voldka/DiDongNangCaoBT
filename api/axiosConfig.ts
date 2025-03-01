852df81378ab1b29471ef796e39177fe59fbff0d
Lê Minh Tài <tailx0913@gmail.com>
2025-03-01 16:16:57 +0700

    s

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:4500'; // Replace 'http://default-api-base-url' with your default base URL if needed
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://192.168.56.1:4500'; // Replace 'http://default-api-base-url' with your default base URL if needed
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
